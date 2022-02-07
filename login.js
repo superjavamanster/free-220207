43471208ef5914526bd2fd01f141220da33cade3
superjavamanster <superjavamanster@gmail.com>
2022-02-07 20:18:38 +0800

    creat-new

---
+++ login.js
@@ -0,0 +1,29 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+
+createApp({
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      user: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    login() {
+      const api = `${this.apiUrl}/admin/signin`;
+
+      axios.post(api, this.user).then((response) => {
+        const { token, expired } = response.data;
+        // 寫入 cookie token
+        // expires 設置有效時間
+        document.cookie = `hexToken=${token};expires=${new Date(expired)}; path=/`;
+
+        window.location = 'index.html';
+      }).catch((error) => {
+        alert(error.data.message);
+      });
+    },
+  },
+}).mount('#app');
